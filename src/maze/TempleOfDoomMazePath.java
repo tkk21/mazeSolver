3553d468c9c68a22f69c9edfa1c0f3cd21d0c585
Ted Kim <tkk21@case.edu>
2014-10-16 20:42:10 -0400

    got rid of unused packages

--- src/maze/TempleOfDoomMazePath.java
+++ src/maze/TempleOfDoomMazePath.java
@@ -1,13 +1,11 @@
 package maze;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 
 /**
  * 
