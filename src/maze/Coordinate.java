4d1c6b8be31ef89000155878fa0a94ba6de3ad46
Ted Kim <tkk21@case.edu>
2014-10-17 10:48:24 -0400

    added javadoc comments

--- src/maze/Coordinate.java
+++ src/maze/Coordinate.java
@@ -2,47 +2,87 @@ package maze;
 
 /**
  * helper class to it abstracts away the x and y component of the coordinate
- * @author ted
+ * @author Ted Kim
  *
  */
 public class Coordinate {
 
+	/**a constant to store what the value of x component of the coordinate is*/
 	private final int x;
+	/**a constant to store what the value of y component of the coordinate is*/
 	private final int y;
 
+	/**
+	 * constructor for coordinate sets the x and y value
+	 * @param x	the x component of the coordinate
+	 * @param y	the y component of the coordinate
+	 */
 	public Coordinate(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * gets the x component of the coordinate
+	 * @return	the x component of the coordinate
+	 */
 	public int getX(){
 		return x;
 	}
 
+	/**
+	 * gets the y component of the coordinate
+	 * @return	the y component of the coordinate
+	 */
 	public int getY(){
 		return y;
 	}
 
+	/**
+	 * checks if the coordinate is in the first quadrant
+	 * @return	if the coordinate is in the first quandrant or not
+	 */
 	public boolean isFirstQuadrant(){
 		return (x>-1 && y>-1);
 	}
 
+	/**
+	 * a helper method to instantiate a coordinate 1 above this one
+	 * @return	a coordinate 1 above this one
+	 */
 	public Coordinate getUpCoordinate(){
 		return new Coordinate(x, y+1);
 	}
 
+	/**
+	 * a helper method to instantiate a coordinate 1 below this one
+	 * @return	a coordinate 1 below this one
+	 */
 	public Coordinate getDownCoordinate(){
 		return new Coordinate(x, y-1);
 	}
 
+	/**
+	 * a helper method to instantiate a coordinate 1 left of this one
+	 * @return	a coordinate 1 left of this one
+	 */
 	public Coordinate getLeftCoordinate(){
 		return new Coordinate(x-1, y);
 	}
 
+	/**
+	 * a helper method to instantiate a coordinate 1 right of this one
+	 * @return	a coordinate 1 right of this one
+	 */
 	public Coordinate getRightCoordinate(){
 		return new Coordinate(x+1, y);
 	}
 
+	/**
+	 * a helper method to instantiate a coordinate 1 to the direction specified
+	 * @param direction	the direction to specify
+	 * @return	a coordinate 1 to the direction specified
+	 */
 	public Coordinate getAdjacentCoordinate (MazeDirection direction){
 		switch (direction){
 		case up:
@@ -58,6 +98,9 @@ public class Coordinate {
 		}
 	}
 	
+	/**
+	 * override of equals so that x and y are compared
+	 */
 	@Override
 	public boolean equals(Object o){
 		Coordinate c = castToCoordinate(o);
@@ -67,6 +110,11 @@ public class Coordinate {
 		return false;
 	}
 
+	/**
+	 * helper method to cast Object into Coordinate
+	 * @param o	Object to cast
+	 * @return	the Object casted into Coordinate
+	 */
 	private Coordinate castToCoordinate(Object o){
 		Coordinate coordinate = null;
 		if (o==null){
