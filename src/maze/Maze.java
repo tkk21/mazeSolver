14651e67d1ed89174a3e428e55b77b418b0ce59b
Ted Kim <tkk21@case.edu>
2014-10-17 10:59:44 -0400

    added more comments and changed tryToPutDownLadder into a void method

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -10,25 +10,50 @@ package maze;
  */
 public class Maze {
 
+	/**the array to represent the maze*/
 	private MazeNode[][] mazeArr;
 	
+	/**
+	 * initializes the maze
+	 * maze is initially full of null nodes
+	 * @param size	the size of the maze
+	 */
 	public Maze (int size){
 		mazeArr = new MazeNode[size][size];
 		//actual nodes are built from the maze builder
 	}
 	
-	MazeNode getNode (Coordinate coordinate){
+	/**
+	 * gets the node at a specified coordinate
+	 * @param coordinate	the coordinate of the node
+	 * @return	the node at the coordinate
+	 */
+	public MazeNode getNode (Coordinate coordinate){
 		return mazeArr[coordinate.getX()][coordinate.getY()];
 	}
 	
-	void setNode (Coordinate coordinate, MazeNode node){
+	/**
+	 * sets the node at a specified coordinate
+	 * @param coordinate	the coordinate of the node
+	 * @param node
+	 */
+	public void setNode (Coordinate coordinate, MazeNode node){
 		mazeArr[coordinate.getX()][coordinate.getY()] = node;
 	}
 	
+	/**
+	 * gets the size of the maze
+	 * @return	the size of the maze
+	 */
 	public int size(){
 		return mazeArr.length;
 	}
 	
+	/**
+	 * checks if the coordinate is within the bounds of the maze
+	 * @param coordinate	the coordinate to check
+	 * @return	if the coordinate is within the bounds of the maze or not
+	 */
 	public boolean isWithinBounds (Coordinate coordinate){
 		return coordinate.isFirstQuadrant() && 
 				coordinate.getX()<size() && 
--- src/maze/MazeBuilder.java
+++ src/maze/MazeBuilder.java
@@ -1,9 +1,21 @@
 package maze;
 
+/**
+ * a helper class to build the maze by
+ * setting nodes to the maze
+ * and setting ladders
+ * @author Ted Kim
+ *
+ */
 public class MazeBuilder {
 
+	/**the maze that is being built*/
 	private Maze maze;
 
+	/**
+	 * constructs a maze and fills it with nodes
+	 * @param size	the size of the maze
+	 */
 	public MazeBuilder (int size){
 		maze = new Maze(size);
 		buildNodes();
@@ -11,13 +23,18 @@ public class MazeBuilder {
 	
 	/**
 	 * sets the ladder boolean as true in both directions
-	 * @param direction
-	 * @param source
+	 * this is done to avoid problems like putting ladder  one way, but forgetting to the other way
+	 * @param direction	the direction to put down the ladder
+	 * @param source	the node the ladder is being put down from
 	 */
 	public void putDownLadder(MazeDirection direction, Coordinate source){
 		tryToPutDownLadder(source, source.getAdjacentCoordinate(direction), direction);
 	}
 	
+	/**
+	 * copies the nodes from an existing maze to this builder
+	 * @param copy	the maze to copy
+	 */
 	public void copyExistingMaze(Maze copy){
 		for (int x = 0;x<copy.size();x++){
 			for (int y = 0;y<copy.size();y++){
@@ -26,10 +43,18 @@ public class MazeBuilder {
 			}
 		}
 	}
+	
+	/**
+	 * returns the maze this builder is building
+	 * @return	the maze this builder is building
+	 */
 	public Maze toMaze(){
 		return maze;
 	}
 	
+	/**
+	 * sets a node to all coordinates in the maze
+	 */
 	private void buildNodes(){
 		for (int x = 0; x<maze.size(); x++){
 			for (int y = 0; y<maze.size(); y++){
@@ -39,12 +64,18 @@ public class MazeBuilder {
 		}
 	}
 	
-	private boolean tryToPutDownLadder(Coordinate from, Coordinate to, MazeDirection direction){
+	/**
+	 * a helper method that checks if the coordinate to put the ladder to is within bounds
+	 * and then puts down the ladder
+	 * @param from	the coordinate to put down the ladder from
+	 * @param to	the coordinate to put down the ladder to
+	 * @param direction	the direction to put down the ladder
+	 */
+	private void tryToPutDownLadder(Coordinate from, Coordinate to, MazeDirection direction){
 		if (!maze.isWithinBounds(to)){
-			return false;
+			return;
 		}
 		maze.getNode(from).putDownLadder(direction);
 		maze.getNode(to).putDownLadder(direction.opposite());
-		return true;
 	}
 }
