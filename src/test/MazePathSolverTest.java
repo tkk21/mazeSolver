bb5b9e96548d680a157b1d2a2565ade3ae2acc58
Ted Kim <tkk21@case.edu>
2014-10-23 13:33:03 -0400

    initial commit. added comments about how to start writing tests

---
+++ src/test/MazePathSolverTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MazePathSolverTest {
+
+	/**
+	 * things to test
+	 * 1. terrible stress test since this solve is O(N^4)
+	 * 2.  
+	 * 
+	 * may need to make some helpers
+	 * external text file based builder?
+	 * 
+	 * Data flow tests
+	 * Test for Defined-Use cases
+	 * use helper methods to see what is expected
+	 * 
+	 * compound boundaries, when there's a comparison
+	 * 	eg. x>1 -> 1. x<1 2. x=1 3. x>1
+	 */
+	@Test
+	public void testShortestPlankPath() {
+		fail("Not yet implemented");
+	}
+
+}
