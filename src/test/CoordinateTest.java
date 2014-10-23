d94fb1ee0c2ff4e9c647f9bd315c54e383e4e560
Ted Kim <tkk21@case.edu>
2014-10-23 13:33:37 -0400

    initial commit of coordinate test

---
+++ src/test/CoordinateTest.java
@@ -0,0 +1,59 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoordinateTest {
+
+	@Test
+	public void testCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsFirstQuadrant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUpCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDownCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLeftCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRightCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAdjacentCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
