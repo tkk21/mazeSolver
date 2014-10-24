5471b9cd87177a3d73cbd9907ce4bbf2455f6087
Ted Kim <tkk21@case.edu>
2014-10-24 04:47:08 -0400

    wrote test outline for comparePath

--- src/test/MazePathSolverTest.java
+++ src/test/MazePathSolverTest.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Queue;
@@ -10,6 +12,7 @@ import maze.Coordinate;
 import maze.Maze;
 import maze.MazeDirection;
 import maze.MazeNode;
+import maze.MazePathSolver;
 
 import org.junit.Test;
 
@@ -18,7 +21,6 @@ public class MazePathSolverTest {
 	/**
 	 * things to test
 	 * 1. terrible stress test since this solve is O(N^4)
-	 * 2.  
 	 * 
 	 * may need to make some helpers
 	 * external text file based builder?
@@ -36,30 +38,80 @@ public class MazePathSolverTest {
 	}
 	
 	/**
-	 * private List<MazeNode> comparePath(List<MazeNode> minPath,
-			List<MazeNode> plankPath) {
+	 * cases for comparePath
+	 * goes through if 
+	 * 1. plankPath != null and plankPath.size<minPath.size -> plankPath becomes minPath
+	 * 2. plankPath != null and minPath == null -> plankPath becomes minPath by default
+	 * doesn't go through if
+	 * 3. plankPath == null -> plankPath never found
+	 * 4. minPath != null and minPath.size<plankPath.size -> minPath smaller than plankPath
+	 * 
+	 * bad data
+	 * 5. null plankPath
+	 * 6. null minPath
+	 * 
+	 * no data flow, nothing's being changed just compared
+	 * 
+	 * boundary
+	 * 7. when minPath.size == plankPath.size
+	 * 
 	 */
+	/**
+	 * Structural Basis
+	 * good data
+	 * compound boundary
+	 * 
+	 */
+	@Test
+	public void testComparePath_plankWinsByComparison() {
+		
+	}
 
-	/**
-	 * private boolean canBeComplete(Coordinate entrance, Coordinate exit, Maze maze) {
-	 */
-	
-	/**
-	 * 	private List<MazeNode> breadthFirstSearch(Maze maze, Coordinate entrance,
-			Coordinate exit){
-	 */
-	
-	/**
-	 * 	private List<MazeNode> createMinPath(HashMap<MazeNode, MazeNode> prevMap, 
-			MazeNode node) {
-	 */
-	
-	/**
-	 * 	private void addAdjacentPlanks(Maze maze, Queue<MazeNode> queue,
-			HashMap<MazeNode, MazeNode> prevMap, MazeNode node) {
-	 */
-	
-	/**
-	 * private boolean canPlacePlank(Maze maze, Coordinate toVisitCoord, MazeDirection direction){
-	 */
+
+	private List<MazeNode> reflectComparePath(List<MazeNode> minPath,
+			List<MazeNode> plankPath) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method method = MazePathSolver.class.getDeclaredMethod
+				("comparePath", List.class, List.class);
+		method.setAccessible(true);
+		return (List<MazeNode>)method.invoke(minPath, plankPath);
+	}
+
+	private boolean reflectCanBeComplete(Coordinate entrance, Coordinate exit, Maze maze) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method method = MazePathSolver.class.getDeclaredMethod
+				("canBeComplete", Coordinate.class, Coordinate.class, Maze.class);
+		method.setAccessible(true);
+		return (boolean)method.invoke(entrance, exit, maze);
+	}
+
+	private List<MazeNode> reflectBreadthFirstSearch(Maze maze, Coordinate entrance,
+			Coordinate exit) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method method = MazePathSolver.class.getDeclaredMethod
+				("breadthFirstSearch", Maze.class, Coordinate.class, Coordinate.class);
+		method.setAccessible(true);
+		return (List<MazeNode>)method.invoke(maze,entrance,exit);
+	}
+
+	private List<MazeNode> reflectCreateMinPath(HashMap<MazeNode, MazeNode> prevMap, 
+			MazeNode node) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		Method method = MazePathSolver.class.getDeclaredMethod
+				("createMinPath", HashMap.class, MazeNode.class);
+		method.setAccessible(true);
+		return (List<MazeNode>)method.invoke(prevMap,node);
+	}
+
+	private void reflectAddAdjacentPlanks(Maze maze, Queue<MazeNode> queue,
+			HashMap<MazeNode, MazeNode> prevMap, MazeNode node) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Method method = MazePathSolver.class.getDeclaredMethod
+				("addAdjacentPlanks", Maze.class, Queue.class, HashMap.class, MazeNode.class);
+		method.setAccessible(true);
+		method.invoke(maze,queue,prevMap,node);
+	}
+
+	private boolean reflectCanPlacePlank(Maze maze, Coordinate toVisitCoord, 
+			MazeDirection direction) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method method = MazePathSolver.class.getDeclaredMethod
+				("canPlacePlank", Maze.class, Coordinate.class, MazeDirection.class);
+		method.setAccessible(true);
+		return (boolean)method.invoke(maze,toVisitCoord,direction);
+	}
 }
