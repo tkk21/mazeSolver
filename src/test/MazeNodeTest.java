da0d3ba06b21245680086319d1cc0a8eb2aeff6f
Ted Kim <tkk21@case.edu>
2014-10-24 02:32:54 -0400

    initial commit

---
+++ src/test/MazeNodeTest.java
@@ -0,0 +1,64 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MazeNodeTest {
+
+	@Test
+	public void testMazeNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasUp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasDown() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsVisited() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVisited() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlaceLadder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasLadder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCopyNode() {
+		fail("Not yet implemented");
+	}
+
+}
