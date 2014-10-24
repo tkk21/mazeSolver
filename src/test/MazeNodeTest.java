77af0f2a37abbd481c34368355c19608e5f1d86e
Ted Kim <tkk21@case.edu>
2014-10-24 02:56:52 -0400

    wrote test for constructor

--- src/test/MazeNodeTest.java
+++ src/test/MazeNodeTest.java
@@ -1,19 +1,63 @@
 package test;
 
 import static org.junit.Assert.*;
+import maze.Coordinate;
+import maze.MazeDirection;
+import maze.MazeNode;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MazeNodeTest {
 
+	MazeNode node;
+	
+	@Before
+	public void reset() {
+		node = new MazeNode(new Coordinate(2, 2));
+	}
+	
+	/**
+	 * constructor simply sets coordinate
+	 * defined and exited
+	 * this is okay since coordinate is a field
+	 * 
+	 * constructor doesn't really have a boundary to deal with
+	 * other classes deal with that
+	 */
+	/**
+	 * structural basis
+	 */
 	@Test
-	public void testMazeNode() {
-		fail("Not yet implemented");
+	public void testMazeNode_nominal() {
+		assertEquals(new Coordinate(2, 2), node.getCoordinate());
+	}
+	
+	/**
+	 * bad data
+	 */
+	@Test
+	public void testMazeNode_nullCoordinate() {
+		
 	}
 
+	/**
+	 * structural basis
+	 * good data
+	 */
 	@Test
 	public void testHasUp() {
-		fail("Not yet implemented");
+		node.placeLadder(MazeDirection.up);
+		assertTrue(node.hasUp());
+	}
+	
+	/**
+	 * structural basis
+	 * good data
+	 */
+	@Test
+	public void testHasUp_false() {
+		assertFalse(node.hasUp());
 	}
 
 	@Test
