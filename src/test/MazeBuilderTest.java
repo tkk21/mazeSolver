41361c92f2433a1fee35899f6e62b01db3ce32bf
Ted Kim <tkk21@case.edu>
2014-10-24 01:02:15 -0400

    initial commit

---
+++ src/test/MazeBuilderTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MazeBuilderTest {
+
+	@Test
+	public void testMazeBuilder() {
+		
+	}
+
+	@Test
+	public void testPlaceLadder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCopyMaze() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToMaze() {
+		fail("Not yet implemented");
+	}
+
+}
